a5d130441a3210eb7cd524229f64cc088a42f7b5
SIB61 <sib.sustswe@gmail.com>
2022-10-18 01:52:25 +0600

    responsive design implemented

--- ui/src/app/ui/navigation/side-navigation/side-navigation.component.ts
+++ ui/src/app/ui/navigation/side-navigation/side-navigation.component.ts
@@ -4,16 +4,19 @@ import { Router, RouterModule } from '@angular/router';
 import { MaterialModule } from '@shared/material/material.module';
 import { Breakpoints } from '@angular/cdk/layout';
 import { BreakPointService } from '@core/services/concrete/break-point/break-point.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faUser, faHashtag, faHome, faPerson, faUserGroup,faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-navigation',
   standalone: true,
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule,FontAwesomeModule],
   templateUrl: './side-navigation.component.html',
   styleUrls: ['./side-navigation.component.scss'],
 })
 export class SideNavigationComponent implements OnInit {
   breakpoints = Breakpoints;
+
   constructor(
     private router: Router,
     public breakpointService: BreakPointService
@@ -30,29 +33,29 @@ export class SideNavigationComponent implements OnInit {
   navList = [
     {
       title: 'Home',
-      icon: 'home',
+      icon: faHome,
       route: '/home/tweet-feed',
     },
     {
       title: 'Explore',
-      icon: 'tag',
+      icon: faHashtag,
       route: '/home/search',
     },
     {
       title: 'Notification',
-      icon: 'notifications',
+      icon: faBell,
       route: '/home/notification',
     },
     {
-      title: 'Profile',
-      icon: 'person',
-      route: '/home/profile',
-    },
-    {
       title: 'Network',
-      icon: 'group',
+      icon: faUserGroup,
       route: '/home/network',
 
+    },
+    {
+      title: 'Profile',
+      icon: faUser,
+      route: '/home/profile',
     },
   ];
 }
