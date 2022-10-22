65225e48e59750de7b2f176dac299a38a1decff9
fahim <mdfahim1194@gmail.com>
2022-10-22 18:35:54 +0600

    user-tweet search

--- ui/src/app/shared/enums/api-endpoint.enum.ts
+++ ui/src/app/shared/enums/api-endpoint.enum.ts
@@ -17,5 +17,9 @@ export const ApiEndpoints = {
   CHANGE_FORGET_PASSWORD:'code-password',
   ADMIN_BLOCK:(id:string):string => `Admin/block/${id}`,
   ADMIN_USER_ById:(id:string)=>`Admin/user/${id}`,
-  ADMIN_USER:`Admin/user`
+  ADMIN_USER:`Admin/user`,
+
+
+  SEARCHUSER: 'Search/users',
+  SEARCHTWEET: 'Search/tweets'
 };
